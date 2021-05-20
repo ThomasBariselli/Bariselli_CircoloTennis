7b6cc96f4f9bec133f9794ae2ecdba70c7bbe552
TB <thomas.bm2003@gmail.com>
2021-05-20 12:52:55 +0200

    inizio creazione menu

--- src/main/java/com/mycompany/bariselli_circolotennis/Main.java
+++ src/main/java/com/mycompany/bariselli_circolotennis/Main.java
@@ -8,8 +8,13 @@ package com.mycompany.bariselli_circolotennis;
 import Eccezioni.EccezioneNonPresente;
 import Eccezioni.EccezioneCodiceNonPresente;
 import Eccezioni.EccezioneDataNonValida;
+import Eccezioni.FileException;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import utility.Menu;
 import utility.Ordinatore;
 
 /**
@@ -20,69 +25,246 @@ public class Main
 {
     public static void main(String[] args) 
     {
-        Maestro m1=new Maestro("Sespo","Mario");
+        String[] vociMenu=new String[8];
+        int sceltaUtente=-1;
+        Scanner tastiera=new Scanner(System.in);
         Circolo c1=new Circolo();
-        Prenotazione p1=new Prenotazione(0, "nome", "cognome", 15, 6, 2021, 13, 0, m1);
-        Prenotazione p2=new Prenotazione(1,"Angelo","Spannung",15,6,2021,14,0,m1);
-        Prenotazione[] prenotazioniCliente1 = new Prenotazione[2];
-        Prenotazione[] prenotazioniCliente;
-        try
+        int esitoOperazione;
+        String caricamentoDaFileOK;
+        String nomeFileTesto="prenotazioniCircolo.txt";
+        String nomeFileBinario="circolo.bin";
+        
+
+        vociMenu[0]="Esci";
+        vociMenu[1]="Aggiungi prenotazione";
+        vociMenu[2]="Rimuovi prenotazione";
+        vociMenu[3]="Visualizzare i dati relativi a tutte le lezioni prenotate da un cliente";
+        vociMenu[4]="Visualizzare tutte le lezioni prenotate per un determinato maestro";
+        vociMenu[5]="Visualizzare  il numero di lezioni tenute da ogni maestro in ordine alfabetico di cliente ";
+        vociMenu[6]="Esporta libri su file CSV";
+        vociMenu[7]="Salva dati";
+    
+          
+        
+        try 
         {
-            c1.aggiungiPrenotazione(p2);
-            c1.aggiungiPrenotazione(p1);
-        }
-        catch(EccezioneDataNonValida e1)
-        {
-            System.out.println(e1.toString());
-        }
-        try
-        {
-            System.out.println("\nPrenotazioni cliente presenti prima dell'eliminazione:\n");
-            prenotazioniCliente=c1.getPrenotazioniCliente("nome","cognome");
-            for(int i=0;i<prenotazioniCliente.length;i++)
-                System.out.println(prenotazioniCliente[i].toString()+"\n");
-            System.out.println("\nPrenotazioni maestro presenti prima dell'eliminazione:\n");
-            prenotazioniCliente1=c1.getPrenotazioniMaestro("Sespo","Mario");
-            for(int i=0;i<prenotazioniCliente1.length;i++)
-                System.out.println(prenotazioniCliente1[i].toString()+"\n");
-        }
-        catch(EccezioneNonPresente e1)
-        {
-            System.out.println(e1.toString());
-        }
-       /* try 
-        {
-            c1.rimuoviPrenotazione(0);
+            c1=c1.caricaCircolo(nomeFileBinario);
+            System.out.println("Dati caricati correttamente");
         } 
-        catch (EccezioneCodiceNonPresente ex) 
+        catch (IOException ex) 
+        {
+            System.out.println("Impossibile accedere al file in lettura. I dati non sono stati caricati");
+        } 
+        catch (FileException ex) 
         {
             System.out.println(ex.toString());
         }
-        try
+        
+        Menu menu= new Menu(vociMenu);
+        
+        do
         {
-            System.out.println("\nPrenotazioni cliente presenti dopo l'eliminazione:\n");
-            Prenotazione[] prenotazioniCliente=c1.getPrenotazioniCliente("nome","cognome");
-            for(int i=0;i<prenotazioniCliente.length;i++)
-                System.out.println(prenotazioniCliente[i].toString()+"\n");
-        }
-        catch(EccezioneNonPresente e1)
-        {
-            System.out.println(e1.toString());
-        }
-        try
-        {
-            System.out.println("\nPrenotazioni maestro presenti dopo l'eliminazione:\n");
-            Prenotazione[] prenotazioniCliente1=c1.getPrenotazioniMaestro("Sespo","Mario");
-            for(int i=0;i<prenotazioniCliente1.length;i++)
-                System.out.println(prenotazioniCliente1[i].toString()+"\n");
-        }
-        catch(EccezioneNonPresente e1)
-        {
-            System.out.println(e1.toString());
-        }*/
-        System.out.println("\nOrdinato:\n");   
-        prenotazioniCliente1=Ordinatore.selectionSortClientiPerMaestroAlfabetico(prenotazioniCliente1);
-        for(int i=0;i<prenotazioniCliente1.length;i++)
-            System.out.println(prenotazioniCliente1[i].toString());
+            try
+            {
+                sceltaUtente=menu.sceltaMenu();
+                switch(sceltaUtente)
+                {
+                    case 0:
+                    {
+                        System.out.println("L'applicazione verrà terminata");
+                        break;
+                    }
+                    case 1:
+                    {
+                        Prenotazione p;
+                        String nome,cognome;
+                        int giorno,mese,anno,ora;
+                        
+                        System.out.println("\nInserisci nome:");
+                        nome=tastiera.nextLine();
+                        System.out.println("Inserisci Cognome:");
+                        cognome=tastiera.nextLine();
+                        System.out.println("Inserisci giorno:");
+                        giorno=tastiera.nextInt();
+                        System.out.println("Inserisci mese:");
+                        mese=tastiera.nextInt();
+                        System.out.println("Inserisci anno:");
+                        anno=tastiera.nextInt();
+                        System.out.println("Inserisci ora:");
+                        ora=tastiera.nextInt();
+                        p=new Prenotazione(ora, nome, cognome, giorno, mese, anno, ora, anno, maestro);
+                        c1.aggiungiPrenotazione(p);
+                        System.out.println("Premi un pulsante per continuare");
+                        tastiera.nextLine();
+                        break;
+                    }
+                    case 2:
+                    {
+                        System.out.println("Ripiano [0..4]-->");
+                        ripiano=tastiera.nextInt();
+                        System.out.println("Posizione [0..14]-->");
+                        posizione=tastiera.nextInt();
+                        try
+                        {
+                            libro=s1.getLibro(ripiano, posizione);
+                            if (libro==null)
+                                System.out.println("Nessun libro presente in questa posizione");
+                            else
+                            {
+                                System.out.println(libro.toString());
+                            }
+                        }
+                        catch (EccezionePosizioneNonValida e1)
+                        {
+                            System.out.println(e1.toString());
+                        }
+
+                        System.out.println("Premi un pulsante per continuare");
+                        tastiera.nextLine();
+                        break;
+                    }
+                    case 3:
+                    {
+                        System.out.println("Ripiano [0..4]-->");
+                        ripiano=tastiera.nextInt();
+                        System.out.println("Posizione [0..14]-->");
+                        posizione=tastiera.nextInt();
+
+                        try
+                        {
+                            s1.rimuoviLibro(ripiano, posizione);
+                            System.out.println("Ok rimozione effettuata correttamente");
+                        }
+                        catch(EccezionePosizioneNonValida e1)
+                        {
+                            System.out.println(e1.toString());
+                        } 
+                        catch (EccezionePosizioneVuota e2) 
+                        {
+                               System.out.println(e2.toString());
+                        }
+                        System.out.println("Premi un pulsante per continuare");
+                        tastiera.nextLine();
+                        break;
+                    }
+                    case 4:
+                    {
+                        String[] elencoTitoli;
+                        String autore;
+                        System.out.println("Autore-->");
+                        autore=tastiera.nextLine();
+                        try
+                        {
+                            elencoTitoli=s1.elencoTitoliAutore(autore);
+                            if (elencoTitoli==null)
+                            System.out.println("Nessun libro presente dell'autore "+autore);
+                            else
+                            {
+                                for (int i=0;i<elencoTitoli.length;i++)
+                                    System.out.println(elencoTitoli[i]);
+                            }
+                        }
+                        catch (EccezionePosizioneNonValida e1)
+                        {
+                            System.out.println(e1.toString());
+                        }
+                        System.out.println("Premi un pulsante per continuare");
+                        tastiera.nextLine();
+                        break;
+                    }
+                    case 5:
+                    {
+                        System.out.println(s1.toString());
+                        break;
+                    }
+                    case 6:
+                    {
+                        try
+                        {
+                            System.out.println(s1.elencoAlfabeticoLibri());
+                        }
+                        catch (EccezionePosizioneNonValida e1)
+                        {
+                            System.out.println(e1.toString());
+                        }
+                        break;
+                    }
+                    case 7:
+                    {
+                        Libro[] elencoLibri;
+                        try
+                        {
+                            elencoLibri=s1.elencoLibriOrdinatiPrezzo();
+                            for (int i=0;i<elencoLibri.length;i++)
+                                System.out.println(elencoLibri[i].toString()+ " € "+elencoLibri[i].prezzo());
+                        }
+                        catch (EccezionePosizioneNonValida e1)
+                        {
+                            System.out.println(e1.toString());
+                        }
+                        break;
+
+                    }
+                    case 8:
+                    {
+                        Libro[] elencoLibri;
+                        try
+                        {
+                            elencoLibri=s1.elencoLibriAlfabeticoAutoreTitolo();
+                            for (int i=0;i<elencoLibri.length;i++)
+                                System.out.println(elencoLibri[i].toString()+ " € "+elencoLibri[i].prezzo()); 
+                        }
+                        catch (EccezionePosizioneNonValida e1)
+                        {
+                            System.out.println(e1.toString());
+                        }
+                        break;
+                    }
+                    case 9:
+                    {
+                        try
+                        {
+                            s1.esportaLibriCSV(nomeFileTesto);
+                            System.out.println("Libri esportati correttamente");
+                        }
+                        catch(IOException e1)
+                        {
+                            System.out.println("Impossibile accedere al file");
+                        }
+                        catch(FileException e2)
+                        {
+                            System.out.println(e2.toString());
+                        }
+                        catch(EccezionePosizioneNonValida e3)
+                        {
+                            System.out.println(e3.toString());
+                        }
+                        break;
+                    }
+                    case 10:
+                    {
+                        try 
+                        {
+                            s1.salvaScaffale(nomeFileBinario);
+                            System.out.println("Dati salvati correttamente");
+                        } catch (IOException ex) 
+                        {
+                            System.out.println("Impossibile accedere al file in scrittura");
+                        }
+
+                     }
+
+                }
+                
+            }
+            catch (InputMismatchException | NumberFormatException e1 )
+            {
+                tastiera.nextLine();
+                System.out.println("Input non corretto");
+            }
+            
+            
+            
+        }while (sceltaUtente!=0);
     }
 }
